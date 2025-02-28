711efa6bb578165910a6874ff87fdee9f5a94f51
i.zacarias <i.zacarias@tu-bs.de>
2025-02-28 18:51:37 +0100

    Adding sFlow support

--- network1.py
+++ network1.py
@@ -4,8 +4,12 @@ import os
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import RemoteController, OVSSwitch
+from mininet.link import Intf
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
+from mininet.util import customClass
+
+# exec(open('sflow.py').read())
 
 class CustomTopology(Topo):
     def build(self):
@@ -53,6 +57,10 @@ class CustomTopology(Topo):
         self.addLink(host1, g1_sw1)
         self.addLink(host2, g3_sw8)
 
+def delete_veth_pairs(host_names):
+    for host in host_names:
+        os.system( 'ip link del root-%s' % host )
+
 def create_veth_pairs(host_names):
     # get machine IP address
     local_ip = os.popen('hostname -I').read().split()[0]
@@ -64,37 +72,65 @@ def create_veth_pairs(host_names):
     for host in host_names:
         # create IP addresses for machines
         mininet_host_ip += 1
-        internal_ip = network[0] + '.' + network[1] + '.' + network[2] + str(mininet_host_ip) + '/32'
+        internal_ip = network[0] + '.' + network[1] + '.' + network[2] + '.' + str(mininet_host_ip) + '/32'
         info("Creating veth pair for %s\n" % host)
         os.system( 'ip link add root-%s type veth peer name %s-root' % (host, host) )
         os.system( 'ip link set root-%s up' % host )
         os.system( 'ip addr add %s dev root-%s' % (vm_ip, host) )
         os.system( 'ip route add %s dev root-%s' % (internal_ip, host) )
 
+def add_external_interfaces(host_names, net):
+    for host in host_names:
+        info("*** Adding interface to root namespace for host %s\n" % host)
+        iface_name = host + '-root'
+        _intf = Intf( iface_name, net.get(host))
+
+def enable_sflow(net):
+    info("*** Enabling sFlow:\n")
+    sflow = 'ovs-vsctl -- --id=@sflow create sflow agent=%s target=%s sampling=%s polling=%s --' % ('enp8s0','127.0.0.1',10,10)
+    for s in net.switches:
+      sflow += ' -- set bridge %s sflow=@sflow' % s
+    info(' '.join([s.name for s in net.switches]) + "\n")
+    os.system(sflow)
 
 def runNetwork():
     setLogLevel('info')      # Set the logging level
     topo = CustomTopology()  # Create the topology
     net = Mininet(topo=topo, switch=OVSSwitch, build=False)  # Set up the network
 
-    # Get all host nodes
-    host_nodes = net.hosts
-    host_names = [host.name for host in host_nodes]
-    create_veth_pairs(host_names)
-
     # Adding External controller
     c0 = net.addController(name='c0', controller=RemoteController, ip='localhost', protocol='tcp',port=6653)
-
-    net.start()
 
     # Connect switches to Controller
     for switch in net.switches:
         switch.start([c0])
 
+    # Build the network (to get access to hosts)
+    # net.build()
+
+    # Get all host nodes    # Get all host nodes
+    host_nodes = net.hosts
+    host_names = [host.name for host in host_nodes]
+    info("*** Host names: %s\n" % host_names)
+    # Create veth pairs for hosts
+    info( '*** Deleting old virtual ethernet pairs\n' )
+    delete_veth_pairs(host_names)
+    info( '*** Creating the virtual ethernet pairs to communicate with the host\n' )
+    create_veth_pairs(host_names)
+
+    # Add network interfaces to the root namespace into Mininet hosts
+    info( '*** Adding interface to root namespace\n')
+    add_external_interfaces(host_names, net)
+
+    enable_sflow(net)
+
+    net.start()
     print("Network is running.")
     CLI(net)  # Start the mininet command line interface
     net.stop()  # Stop the network when done
 
+    info( '*** Deleting old virtual ethernet pairs\n' )
+    delete_veth_pairs(host_names)
 
 if __name__ == '__main__':
     runNetwork()
