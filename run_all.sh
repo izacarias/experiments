d0b7508b6b91c5862c0c448e38cca20b974e68fb
i.zacarias <i.zacarias@tu-bs.de>
2025-05-12 18:54:13 +0200

    Adding scenarios with loss from 0.5 to 4.0 percent of packets drops

---
+++ run_all.sh
@@ -0,0 +1,9 @@
+# Run all experiments with link loss probability
+# sudo INFLUXDB_BUCKET=dtn05 ./network4.py-loss-05.py
+# sudo INFLUXDB_BUCKET=dtn10 ./network4.py-loss-10.py
+# sudo INFLUXDB_BUCKET=dtn15 ./network4.py-loss-15.py
+sudo INFLUXDB_BUCKET=dtn20 ./network4.py-loss-20.py
+sudo INFLUXDB_BUCKET=dtn25 ./network4.py-loss-25.py
+sudo INFLUXDB_BUCKET=dtn30 ./network4.py-loss-30.py
+sudo INFLUXDB_BUCKET=dtn35 ./network4.py-loss-35.py
+sudo INFLUXDB_BUCKET=dtn40 ./network4.py-loss-40.py
