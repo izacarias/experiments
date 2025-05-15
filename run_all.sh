8907fc55afb53be91825d7722af04a8d5fe07edc
i.zacarias <i.zacarias@tu-bs.de>
2025-05-15 18:07:42 +0200

    Removing old scenarios

--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,9 @@
 # Run all experiments with link loss probability
-# sudo INFLUXDB_BUCKET=dtn05 ./network4.py-loss-05.py
-# sudo INFLUXDB_BUCKET=dtn10 ./network4.py-loss-10.py
-# sudo INFLUXDB_BUCKET=dtn15 ./network4.py-loss-15.py
-sudo INFLUXDB_BUCKET=dtn20 ./network4.py-loss-20.py
-sudo INFLUXDB_BUCKET=dtn25 ./network4.py-loss-25.py
-sudo INFLUXDB_BUCKET=dtn30 ./network4.py-loss-30.py
-sudo INFLUXDB_BUCKET=dtn35 ./network4.py-loss-35.py
-sudo INFLUXDB_BUCKET=dtn40 ./network4.py-loss-40.py
+sudo INFLUXDB_BUCKET=dtn05 LINK_LOSS=0.5 ./network.py
+sudo INFLUXDB_BUCKET=dtn10 LINK_LOSS=1.0 ./network.py
+sudo INFLUXDB_BUCKET=dtn15 LINK_LOSS=1.5 ./network.py
+sudo INFLUXDB_BUCKET=dtn20 LINK_LOSS=2.0 ./network.py
+sudo INFLUXDB_BUCKET=dtn25 LINK_LOSS=2.5 ./network.py
+sudo INFLUXDB_BUCKET=dtn30 LINK_LOSS=3.0 ./network.py
+sudo INFLUXDB_BUCKET=dtn35 LINK_LOSS=3.5 ./network.py
+sudo INFLUXDB_BUCKET=dtn40 LINK_LOSS=4.0 ./network.py
