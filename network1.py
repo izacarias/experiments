b38156d3abda57eb07b52a67b7e60d38de91a3fd
i.zacarias <i.zacarias@tu-bs.de>
2025-05-15 17:20:12 +0200

    Updating template scenario

--- network1.py
+++ network1.py
@@ -14,51 +14,17 @@ from mininet.log import setLogLevel, info, debug
 from mininet.util import customClass
 from dotenv import load_dotenv
 from influxdb_client import InfluxDBClient, WriteOptions, Point
+from utils.onos_client import onos_get_links, onos_get_port_stats, onos_get_link_usage, onos_link_history
+from utils.sflowrt_client import define_sflowrt_flow, get_flow_value, sflow_flows
 
 # Load environment variables from .env file
 load_dotenv()
 
 # sflow-r configuration
-sflowrt_url = os.getenv('SFLOWRT_URL', 'http://localhost:8008')
 influxdb_url = os.getenv('INFLUXDB_URL', 'http://localhost:8086')
 influxdb_token = os.getenv('INFLUXDB_TOKEN')
 influxdb_org = os.getenv('INFLUXDB_ORG')
 influxdb_bucket = os.getenv('INFLUXDB_BUCKET')
-# ONOS
-onos_url = os.getenv('ONOS_URL', 'http://localhost:8181'    )
-onos_username = os.getenv('ONOS_USERNAME', 'onos')
-onos_password = os.getenv('ONOS_PASSWORD', 'rocks')
-
-# Add historical data storage
-onos_link_history = {}  # Format: {(device_id, port_id): {'bytes': value, 'duration': value}}
-
-# Definition of sflow-rt- flows
-sflow_flows = {
-    "tcprtt": {
-        "keys": "ipsource,ipdestination,tcpsourceport,tcpdestinationport",
-        "value": "tcprtt"
-    },
-    "tcpcwndsnd": {
-        "keys": "ipsource,ipdestination,tcpsourceport,tcpdestinationport",
-        "value": "tcpcwndsnd"
-    },
-    "tcpwindow": {
-        "keys": "ipsource,ipdestination,tcpsourceport,tcpdestinationport",
-        "value": "tcpwindow"
-    },
-    "tcplost": {
-        "keys": "ipsource,ipdestination,tcpsourceport,tcpdestinationport",
-        "value": "tcplost"
-    },
-    "tcpretrans": {
-        "keys": "ipsource,ipdestination,tcpsourceport,tcpdestinationport",
-        "value": "tcpretrans"
-    },
-    "tcpunacked": {
-        "keys": "ipsource,ipdestination,tcpsourceport,tcpdestinationport",
-        "value": "tcpunacked"
-    }
-}
 
 # Enables sflow monitoring on OVS Switches
 exec(open('sflow.py').read())
@@ -66,95 +32,6 @@ exec(open('sflow.py').read())
 # Initialize InfluxDB client
 influxdb_client = InfluxDBClient(url=influxdb_url, token=influxdb_token, org=influxdb_org)
 influxdb_write_api = influxdb_client.write_api(write_options=WriteOptions(batch_size=1))
-
-
-# Get links from ONOS
-def onos_get_links():
-    url = '%s/onos/v1/links' % onos_url
-    response = requests.get(url, auth=HTTPBasicAuth(onos_username, onos_password))
-    if response.status_code == 200:
-        return response.json()
-    else:
-        raise Exception(f"Failed to get links from ONOS: {response.status_code} {response.text}")
-
-
-# Get link usage from ONOS
-def onos_get_port_stats(device_id, port_id):
-    url = "%s/onos/v1/statistics/ports/%s/%s" % (onos_url, device_id, port_id)
-    response = requests.get(url, auth=HTTPBasicAuth(onos_username, onos_password))
-    response.raise_for_status()
-    return response.json()
-
-
-def onos_get_link_usage():
-    global onos_link_history
-    return_value = []
-    links = onos_get_links()
-    for link in links['links']:
-        src_device = link['src']['device']
-        src_port = link['src']['port']
-        dst_device = link['dst']['device']
-        
-        # Get statistics for the source device
-        src_stats = onos_get_port_stats(src_device, src_port)
-        current_bytes = src_stats.get('statistics')[0].get('ports')[0].get('bytesSent')
-        current_duration = src_stats.get('statistics')[0].get('ports')[0].get('durationSec')
-        
-        # Get historical data
-        link_key = (src_device, src_port)
-        prev_stats = onos_link_history.get(link_key, {'bytes': current_bytes, 'duration': current_duration})
-        
-        # Calculate delta values
-        bytes_delta = current_bytes - prev_stats['bytes']
-        duration_delta = current_duration - prev_stats['duration']
-        
-        # Calculate data rate
-        datarate = (bytes_delta * 8) / duration_delta if duration_delta > 0 else 0
-        
-        # Update historical data
-        onos_link_history[link_key] = {
-            'bytes': current_bytes,
-            'duration': current_duration
-        }
-        debug("Datarate for %s to %s: %d bps \n" % (src_device, dst_device, datarate))
-        return_value.append((src_device, dst_device, datarate))
-    return return_value
-
-
-# Define sflow-rt flow if not exists
-def define_sflowrt_flow():
-    for flow, flow_definition in sflow_flows.items():
-        flow_url = '%s/flow/%s/json' % (sflowrt_url, flow)
-        debug("Quering for sflow flow definition "+ flow)
-        response = requests.get(flow_url)
-        if response.status_code == 404:
-            response = requests.put(flow_url, json=flow_definition)
-            if response.status_code in [200, 204]:
-                print("sflow-rt flow defined successfully")
-            else:
-                raise Exception(f"Failed to define sflow-rt flow: {response.status_code} {response.text}")
-                
-        else:
-            print("sflow-rt: flow " + flow + " already exists")
-
-
-# Get flow value
-def get_flow_value(flow, src_host, dst_host):
-    flow_url = '%s/activeflows/ALL/%s/json' % (sflowrt_url, flow)
-    response = requests.get(flow_url)
-    if response.status_code == 200:
-        for flow_item in response.json():
-            key = flow_item.get('key')
-            key_parts = key.split(',')
-            if key_parts[0] == src_host and key_parts[1] == dst_host:
-                flow_value = flow_item.get('value')
-                debug("sflow-rt value for flow %s(src: %s dst: %s): %s\n" % (flow, src_host, dst_host, flow_value))
-                return flow_value
-        
-    else:
-        info("Failed to get flow %s value: %s %s" % (flow, response.status_code, response.text))
-        return None
-
 
 def export_to_influxdb(stop_event):
     while not stop_event.is_set():
@@ -250,6 +127,8 @@ def configure_host_ip(net):
 
 class CustomTopology(Topo):
     def build(self):
+        # Defining the link loss
+        LINK_LOSS=0.0
         # Create switches for group g1
         g1_sw1 = self.addSwitch('s1', protocols="OpenFlow13")
         g1_sw2 = self.addSwitch('s2', protocols="OpenFlow13")
@@ -266,19 +145,19 @@ class CustomTopology(Topo):
         g3_sw9 = self.addSwitch('s9', protocols="OpenFlow13")
 
         # Connect switches within group g1
-        self.addLink(g1_sw1, g1_sw2, bw=99)    # l_1
-        self.addLink(g1_sw2, g1_sw3, bw=96)    # l_2
-        self.addLink(g1_sw1, g1_sw3, bw=100)    # l_3
+        self.addLink(g1_sw1, g1_sw2, bw=99, loss=LINK_LOSS)    # l_1
+        self.addLink(g1_sw2, g1_sw3, bw=96, loss=LINK_LOSS)    # l_2
+        self.addLink(g1_sw1, g1_sw3, bw=100, loss=LINK_LOSS)   # l_3
         
         # Connect switches within group g2
-        self.addLink(g2_sw4, g2_sw5, bw=93)    # l_4
-        self.addLink(g2_sw5, g2_sw6, bw=100)    # l_5
-        self.addLink(g2_sw4, g2_sw6, bw=95)    # l_6
+        self.addLink(g2_sw4, g2_sw5, bw=93, loss=LINK_LOSS)    # l_4
+        self.addLink(g2_sw5, g2_sw6, bw=100, loss=LINK_LOSS)   # l_5
+        self.addLink(g2_sw4, g2_sw6, bw=95, loss=LINK_LOSS)    # l_6
         
         # Connect switches within group g3
-        self.addLink(g3_sw7, g3_sw8, bw=89)    # l_7
-        self.addLink(g3_sw8, g3_sw9, bw=91)    # l_8
-        self.addLink(g3_sw7, g3_sw9, bw=97)    # l_9
+        self.addLink(g3_sw7, g3_sw8, bw=89, loss=LINK_LOSS)    # l_7
+        self.addLink(g3_sw8, g3_sw9, bw=91, loss=LINK_LOSS)    # l_8
+        self.addLink(g3_sw7, g3_sw9, bw=97, loss=LINK_LOSS)    # l_9
 
         # Connect each group via links
         # g1 to g2
@@ -343,14 +222,15 @@ def runNetwork():
 
     info("*** Start Iperf client on host h1\n")
     h1 = net.get('h1')
-    h1.cmd('iperf -c 10.0.0.2 -t 300 &')
+    h1.cmd('iperf -c 10.0.0.2 -t 900 &')
     
 
-    time.sleep(60)
-    info("*** Running Mininet CLI\n")
-    CLI(net)  # Start the mininet command line interface
+    time.sleep(910)
+    # info("*** Running Mininet CLI\n")
+    # CLI(net)  # Start the mininet command line interface
     
     # Stop thread
+    info("*** Stopping Mininet \n")
     stop_event.set()
     export_thread.join(timeout=3)
 
